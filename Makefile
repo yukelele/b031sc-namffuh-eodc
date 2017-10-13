10064755323fa9d5ff77ca89b4ae1c30bde07e1e
Yuki Mano <ysm@umail.ucsb.edu>
2017-10-13 10:03:50 -0700

    Makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+CXX=g++
+
+CXXFLAGS = -std=c++11 -Wall
+
+all: frequencies
+
+frequencies: frequencies.o
+	${CXX} $^ -o $@
+
+clean:
+	/bin/rm -f prog1 *.o *~
--- frequencies.cpp
+++ frequencies.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std; 
 
 int main() {
-
+  
   string line;
-  ifstream myfile ("test1.in");
+  ifstream myfile ("test_files/test1.in");
   if (myfile.is_open())
     {
       while ( getline (myfile,line) )
 	{
 	  cout << line << '\n';
+	  cout << line.length() << endl;
+	  cout << line[0] << endl;
 	}
       myfile.close();
     }
 
   else cout << "Unable to open file"; 
-
+  
   return 0; 
 }
