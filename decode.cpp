f42cbd3099b827b12ebdbe090bc312c0fa96a3fb
Yuki Mano <ysm@umail.ucsb.edu>
2017-10-17 12:36:58 -0700

    -_-

--- decode.cpp
+++ decode.cpp
@@ -30,89 +30,68 @@ int main (int argc, char* argv[]){
       mapOfEncodedAscii[huffman_value] = ascii_value;  // key is huffman value & value is ascii
     }    
   
-    int line = 0;
-
   string binary_string = "";
   int bit_count = 0;
-  char c;
-  char d;
+  unsigned char c;
+  unsigned char d;
   while (!cin.eof())
-    {   
+  {   
         c = cin.get();
+        
         d = cin.get(); 
-        char e = cin.peek();
         
-
-        //cout << decimal_to_binary(int(c)) << endl;
-        //cout << decimal_to_binary(int(e)) << endl;
-        //cout << int('\xff') << endl;
-/*
-        char e2 = cin.get();
-        char e3 = cin.get();
-        char e4 = cin.get();
-        char e5 = cin.get();
-        char e6 = cin.get();
-        char e7 = cin.get();
-*/
-//cout <<"peeking value is " << e << endl;
         
-        if(e!=EOF){ //&& (e2!=EOF) && (e3!=EOF) && (e4!=EOF) && (e5!=EOF) && (e6!=EOF) && (e7!=EOF)){
+        if(cin.peek()!=EOF)
+        { 
         
-        cin.putback(d);
-        /*
-        cin.putback(e2);
-        cin.putback(e3);
-         cin.putback(e4);
-         cin.putback(e5);
-            cin.putback(e6);
-             cin.putback(e7);
-       */
+          cin.putback(d);
+     
+          binary_string += decimal_to_binary(int(c)); 
+          bit_count += binary_string.length();
 
-        binary_string += decimal_to_binary(int(c)); 
-        bit_count += binary_string.length();
+          int i=1;
+          bool loop = true;
+          while(bit_count!=0 && loop){
+            if(i == bit_count) loop = false; 
 
-        int i=1;
-        bool loop = true;
-        while(loop){
-          if(i == bit_count) loop = false; 
+            if(mapOfEncodedAscii.count(binary_string.substr(0, i)) == 1){
+              int asciiVal = stoi(mapOfEncodedAscii[binary_string.substr(0,i)]); 
+              char asciiChar = asciiVal;
+              
+              //cout << "string : " << binary_string.substr(0,i) << " = ";
+              //cout << "int : " << asciiVal << " == ";
+              //cout << "char : ";
+              cout << asciiChar;
+              //cout << " ||| " ;
 
-          if(mapOfEncodedAscii.count(binary_string.substr(0, i)) == 1){
-            int asciiVal = stoi(mapOfEncodedAscii[binary_string.substr(0,i)]); 
-            char asciiChar = asciiVal;
+              bit_count -= i;
+              binary_string = binary_string.substr(i, bit_count); 
+              i = 1;          
+            }
 
-            line++;
-//cout << line << ". the character is : " << asciiChar << endl; //MODIFY THIS LINE
-            cout << asciiChar;
-
-            //cout << endl; // DELETE LINE
-
-            bit_count -= i;
-            binary_string = binary_string.substr(i, bit_count); 
-            i = 1;          
-          }
-
-          else{
-            i++;
+            else{
+              i++;
+            }
           }
         }
-        }
-        else
-          break;
-    }
-   
- 
-    binary_string += decimal_to_binary(int(c)).substr(0,8-int(d));
+        else{
+          binary_string += decimal_to_binary(int(c)).substr(0,8-int(d));
 
     bit_count = binary_string.length();
     int i=1;
     bool loop = true;
-    while(loop){
+    while(bit_count!=0 && loop){
       if(i == bit_count) loop = false; 
 
       if(mapOfEncodedAscii.count(binary_string.substr(0, i)) == 1){
         int asciiVal = stoi(mapOfEncodedAscii[binary_string.substr(0,i)]); 
         char asciiChar = asciiVal;
-        cout << asciiChar;
+        //cout << "int value is : "<< mapOfEncodedAscii[binary_string.substr(0,i)] << endl;
+        //cout << asciiChar;
+         //cout << "the last extra int:  " << asciiVal << " == ";
+           //   cout << "char: ";
+              cout << asciiChar;
+             // cout << " ||| " ;
 
         bit_count -= i;
         binary_string = binary_string.substr(i, bit_count); 
@@ -122,11 +101,13 @@ int main (int argc, char* argv[]){
       else{
         i++;
       }
+        }
+          //break;
     }
-
-    //cout << endl; // delete line e
-
-    //cout << decimal_to_binary(int(cin.get())) << endl;
+   
+ 
+    
+    }
 
   return 0;
 }
